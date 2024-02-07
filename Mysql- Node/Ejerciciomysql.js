195460c2fe253e18e1d6c77d52fa758eae981e0f
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2024-02-07 09:42:51 -0500

    Add files via upload

---
+++ Mysql- Node/Ejerciciomysql.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql'); //asignamos mysql  una variable con el mismo nombre
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected");
+});
